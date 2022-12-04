bd5150a0debb2b4f06a3e0b792b586f565ab579d
Benny <u3569037@connect.hku.hk>
2022-12-04 18:14:32 +0800

    remaked version by benny

--- app/src/main/java/com/example/audiolecture/MainActivity.kt
+++ app/src/main/java/com/example/audiolecture/MainActivity.kt
@@ -7,29 +7,38 @@ import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var mediarec : MediaRecorder
+    private lateinit var mediarec : MediaRecorder
+    private lateinit var btnStart: Button
+    private lateinit var btnStop: Button
+    private lateinit var btnPlay: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnStart = findViewById<Button>(R.id.btnStart)
-        val btnStop = findViewById<Button>(R.id.btnStop)
-        val btnPlay = findViewById<Button>(R.id.btnPlay)
+        btnStart = findViewById<Button>(R.id.btnStart)
+        btnStop = findViewById<Button>(R.id.btnStop)
+        btnPlay = findViewById<Button>(R.id.btnPlay)
 
-        val path: String = getExternalFilesDir("").toString() + "/myrec.3gp"
-        mediarec = MediaRecorder()
+        //disable the buttons
         btnStart.isEnabled = false
         btnStop.isEnabled = false
-        //btnPlay.isEnabled = false
+
+        val date = SimpleDateFormat("yyyy-MM-dd HH:mm").format(Date())
+        val filename = "record_$date.3gp"
+        val path: String = getExternalFilesDir("").toString() + "/$filename"
+        mediarec = MediaRecorder()
 
 
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO,
-                                                                        Manifest.permission.WRITE_EXTERNAL_STORAGE),111)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE),111)
         btnStart.isEnabled = true
 
 
@@ -39,12 +48,18 @@ class MainActivity : AppCompatActivity() {
             mediarec.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
             mediarec.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
             mediarec.setOutputFile(path)
-            mediarec.prepare()
+            try {
+                mediarec.prepare()
+            } catch (e: IOException) {
+                Toast.makeText(
+                    this,
+                    e.toString(),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
             mediarec.start()
             btnStart.isEnabled = false
             btnStop.isEnabled = true
-
-
         }
 
 
@@ -53,7 +68,6 @@ class MainActivity : AppCompatActivity() {
             btnStop.isEnabled = false
             btnStart.isEnabled = true
             mediarec.stop()
-
         }
 
         //play recording
@@ -71,12 +85,6 @@ class MainActivity : AppCompatActivity() {
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-
-        val btnStart = findViewById<Button>(R.id.btnStart)
-        val btnStop = findViewById<Button>(R.id.btnStop)
-        val btnPlay = findViewById<Button>(R.id.btnPlay)
-
-
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if(requestCode == 111 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
             btnStart.isEnabled = true
